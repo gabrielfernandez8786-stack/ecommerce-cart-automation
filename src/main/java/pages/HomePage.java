0a47999878893bf1e4356e9f9f0ecb45c56c4ca3
Dharo Gabriel Fernandez <gabriel.fernandez8786@gmail.com>
2026-05-20 18:16:27 -0500

    Update and rename HomePage.java
    Se corrige Test Home page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,14 +1,21 @@
 package pages;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
-    @FindBy(name="search") private WebElement searchBox;
-    @FindBy(css="button[type='submit']") private WebElement searchBtn;
+
+    @FindBy(name = "search")
+    private WebElement searchBox;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement searchBtn;
 
     public HomePage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public void openHome() {
